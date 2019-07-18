af28fc261fce77cbe2d185c8c429656962755c5f
CaptainJ <ftang2011@outlook.com>
2019-07-18 17:11:58 +0800

    Update test-tor.py

--- test-tor.py
+++ test-tor.py
@@ -7,35 +7,37 @@ import time
 import requests
 import console
 
-
-def TorIPChanger():
-    def __init__(self):
-        TOR_CONTROL_PORT = 9051
-        TOR_DATA_PORT = 9050
-        PASSWORD = '12345'
-        self.controller = Controller.from_port(port=TOR_CONTROL_PORT)
-        self.controller.authenticate(PASSWORD)
-
-        socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5,
+TOR_CONTROL_PORT = 9051
+TOR_DATA_PORT = 9050
+PASSWORD = '12345'
+        
+def init():
+    print('init...')
+    controller = Controller.from_port(port=TOR_CONTROL_PORT)
+    controller.authenticate(PASSWORD)
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5,
                               '127.0.0.1',
                               TOR_DATA_PORT,
                               True)
-        socket.socket = socks.socksocket
+    socket.socket = socks.socksocket
+    print('init over')
+    return controller
 
-    def change_ip(self):
+def change_ip(controller):
+    if True:
         console.info('Waiting for new ip...')
-        self.controller.signal(Signal.NEWNYM)
-        time.sleep(self.controller.get_newnym_wait())
+        controller.signal(Signal.NEWNYM)
+        time.sleep(controller.get_newnym_wait())
         console.info('IP changed')
 
 
 if __name__ == '__main__':
-    ipchanger = TorIPChanger()
+    controller = init()
 
     url = 'http://ipecho.net/plain'
     res = requests.get(url)
     print(res.text)
 
-    ipchanger.change_ip()
+    change_ip(controller)
     res = requests.get(url)
     print(res.text)
